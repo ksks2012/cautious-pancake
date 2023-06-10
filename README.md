86c22897125065d06896b710570c3fca49ba1948
ksks2012 <st890751999@gmail.com>
2023-06-10 23:30:20 +0800

    feat(init): init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+For analysis of bp
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup  # pylint: disable=import-error
+from setuptools import find_packages
+
+setup(name="bpsupport",
+		version="0.0.1",
+		description="An simple program for support bp",
+		packages=find_packages(),
+		install_requires=[
+			"PyYaml >= 6.0",
+            "requests >= 2.21.0",
+            "beautifulsoup >= 4 4.12.2"
+		],
+		entry_points={
+				'console_scripts': [
+						'out_to_csv = albionsupport.cmd.out_to_csv:main',
+				],
+		},
+		classifiers=[
+				"Development Status :: 3 - Alpha",
+				"Intended Audience :: Developers",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.10.6",
+		],
+		)
+
+# vim: tabstop=4 shiftwidth=4
