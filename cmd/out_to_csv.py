04f07e03a3e0191963f6a05a37269eb42bd8238f
ksks2012 <st890751999@gmail.com>
2023-06-11 22:52:47 +0800

    fix(out_to_csv): clear import

--- cmd/out_to_csv.py
+++ cmd/out_to_csv.py
@@ -1,11 +1,4 @@
-import pprint
-import sys
-from api_routine.api import get_timely_price
-from utils.read_file import read_yaml
-from utils import path
-
-
-def main(config_path=None):
+def main():
     pass 
 
 if __name__ == '__main__':
