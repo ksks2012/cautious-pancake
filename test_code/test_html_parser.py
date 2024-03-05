653888d4fc2f88fbd4125c11ef92fd60d1255d19
ksks2012 <st890751999@gmail.com>
2024-03-05 21:16:22 +0800

    test(test_html_parser):
    - Refactor import
    - Add test_list_draft

--- test_code/test_html_parser.py
+++ test_code/test_html_parser.py
@@ -1,26 +1,30 @@
 import pprint
 
 from utils import file_processor
-from utils.html_parser import analysis_control, list_game_table, list_improvement_jumps, list_salary
+from utils import html_parser
 from utils.text import INPUT
 
 def test_analysis_control():
     rows = file_processor.read_json(f"{INPUT}_table.json")
     ids = file_processor.read_json(f"{INPUT}_ids.json")
     
-    analysis_control(rows, ids)
+    html_parser.analysis_control(rows, ids)
 
 def test_list_game_table():
-    rows, ids = list_game_table()
+    rows, ids = html_parser.list_game_table()
     pprint.pprint(rows)
     pprint.pprint(ids)
 
 def test_list_improvement_jumps():
-    rows = list_improvement_jumps()
+    rows = html_parser.list_improvement_jumps()
     pprint.pprint(rows)
 
 def test_list_salary():
-    rows = list_salary()
+    rows = html_parser.list_salary()
+    pprint.pprint(rows)
+
+def test_list_draft():
+    rows = html_parser.list_draft()
     pprint.pprint(rows)
 
 if __name__ == '__main__':
@@ -28,3 +32,4 @@ if __name__ == '__main__':
     test_analysis_control()
     test_list_improvement_jumps()
     test_list_salary()
+    test_list_draft()
