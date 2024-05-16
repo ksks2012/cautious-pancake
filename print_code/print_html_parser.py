eb6693330be8029876c9fa3051db6658227adbf9
ksks2012 <st890751999@gmail.com>
2024-05-16 17:58:57 +0800

    feat(print_html_parser):
    - Add print_match_abilities
    - Rename test_* to print_*

--- print_code/print_html_parser.py
+++ print_code/print_html_parser.py
@@ -1,36 +1,43 @@
 import pprint
-import unittest
 
 from utils import file_processor
 from utils import html_parser
 from utils.text import INPUT
 
-def test_analysis_control():
+def print_analysis_control():
     rows = file_processor.read_json(f"{INPUT}_table.json")
     ids = file_processor.read_json(f"{INPUT}_ids.json")
     
     html_parser.analysis_control(rows, ids)
 
-def test_list_game_table():
+def print_list_game_table():
     rows, ids = html_parser.list_game_table()
     pprint.pprint(rows)
     pprint.pprint(ids)
 
-def test_list_improvement_jumps():
+def print_list_improvement_jumps():
     rows = html_parser.list_improvement_jumps()
     pprint.pprint(rows)
 
-def test_list_salary():
+def print_list_salary():
     rows = html_parser.list_salary()
     pprint.pprint(rows)
 
-def test_list_draft():
+def print_list_draft():
     rows = html_parser.list_draft()
     pprint.pprint(rows)
 
+def print_match_abilities():
+    with open(f"./var/player-test.html", "rb") as fr:
+        response = fr.read()
+    
+    rows = html_parser.match_abilities(response)
+    pprint.pprint(rows)
+
 if __name__ == '__main__':
-    test_list_game_table()
-    test_analysis_control()
-    test_list_improvement_jumps()
-    test_list_salary()
-    test_list_draft()
+    print_list_game_table()
+    print_analysis_control()
+    print_list_improvement_jumps()
+    print_list_salary()
+    print_list_draft()
+    print_match_abilities()
